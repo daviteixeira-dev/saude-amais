d9c267799efa3627079b133be393bccf2edf8123
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-10-25 22:24:51 -0300

    Componente de Modalidades - Davi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Header from "./components/Header";
-
-import "./global.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container } from "@mui/material/";
+import PagePilates from "./pages/PagePilates";
+import Modalidades from "./components/Modalidades/Modalidades";
+//import styles from "./index.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <h1>Saude a mais</h1>
-    </>
+    <Router>
+      <Switch>
+        <Container maxWidth="lg">
+          <Route exact path="/">
+            <Modalidades />
+          </Route>
+          <Route path="/modalidades">
+            <PagePilates />
+          </Route>
+        </Container>
+      </Switch>
+    </Router>
   );
 }
 
